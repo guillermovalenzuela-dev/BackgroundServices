2dd1fb78bc9eb7dea027d33105a8a06b1196653d
GUILLERMO VALENZUELA <kaoscore53@yahoo.com>
2023-07-20 18:25:54 -0500

    Add project files.

---
+++ SimpleBackground/BackgroundServices/PeriodicService.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace SimpleBackground.BackgroundServices;
+public class PeriodicService : BackgroundService
+{
+    private readonly ILogger<PeriodicService> logger;
+    private readonly TimeSpan period = TimeSpan.FromSeconds(5);
+    public PeriodicService(ILogger<PeriodicService> logger)
+    {
+        this.logger = logger;
+    }
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using PeriodicTimer timer = new PeriodicTimer(period);
+
+        while (!stoppingToken.IsCancellationRequested &&
+               await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            logger.LogInformation("Executing PeriodicBackgroundTask");
+        }
+    }
+}
---
+++ SimpleBackground/BackgroundServices/SimpleService.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace SimpleBackground.BackgroundServices;
+public class SimpleService : BackgroundService
+{
+    private readonly ILogger<SimpleService> logger;
+    public SimpleService(ILogger<SimpleService> logger)
+    {
+        this.logger = logger;
+    }
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var numberOfThreads = Process.GetCurrentProcess().Threads.Count;
+
+        logger.LogInformation($"Task executed. Numer of Threads: {numberOfThreads}");
+        await Task.CompletedTask;
+    }
+}
